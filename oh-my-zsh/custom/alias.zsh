26a6ac0a59cf1bd70c5f8de70577b3c2b86bd24a
David Wretman <david@wretman.se>
2024-04-18 15:24:24 +0200

    Add oh-my-zsh aliases file

---
+++ oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,13 @@
+picoterm(){
+  picocom $1 \
+    -b 115200 -e q --omap crlf \
+    -s "ascii-xfr -s -n" \
+    --receive-cmd "ascii-xfr -r -n" \
+    --flow n
+}
+
+# Set surrent value of SSH_AUTH_SOCK, use to get ssh agent forwarding working
+# in e.g. tmux after ssh reconnection 
+fixssh() {
+  eval $(tmux show-env -s |grep '^SSH_')
+}
